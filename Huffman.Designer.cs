840900aa9bd95608d95a0f3d91a5ef8ef5f43c2c
CoderAndyLive <116788767+CoderAndyLive@users.noreply.github.com>
2024-02-28 10:31:06 +0100

    Initial commit

---
+++ Huffman.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace HuffmanCode
+{
+    partial class Huffman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            RB_Dekomprimieren = new RadioButton();
+            RB_Komprimieren = new RadioButton();
+            BTN_Help = new PictureBox();
+            BTN_Back = new PictureBox();
+            BTN_Start = new PictureBox();
+            TB_Output = new TextBox();
+            TB_Input = new TextBox();
+            TB_PathIn = new TextBox();
+            TB_PathOut = new TextBox();
+            LBL_Eingabe = new Label();
+            LBL_Ausgabe = new Label();
+            LBL_Titel = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            checkBox1 = new CheckBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)BTN_Help).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BTN_Back).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)BTN_Start).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(RB_Dekomprimieren);
+            groupBox1.Controls.Add(RB_Komprimieren);
+            groupBox1.Location = new Point(152, 153);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(490, 77);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // RB_Dekomprimieren
+            // 
+            RB_Dekomprimieren.AutoSize = true;
+            RB_Dekomprimieren.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            RB_Dekomprimieren.Location = new Point(210, 17);
+            RB_Dekomprimieren.Name = "RB_Dekomprimieren";
+            RB_Dekomprimieren.Size = new Size(223, 39);
+            RB_Dekomprimieren.TabIndex = 1;
+            RB_Dekomprimieren.Text = "Dekomprimieren";
+            RB_Dekomprimieren.UseVisualStyleBackColor = true;
+            // 
+            // RB_Komprimieren
+            // 
+            RB_Komprimieren.AutoSize = true;
+            RB_Komprimieren.Checked = true;
+            RB_Komprimieren.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            RB_Komprimieren.Location = new Point(10, 17);
+            RB_Komprimieren.Name = "RB_Komprimieren";
+            RB_Komprimieren.Size = new Size(194, 39);
+            RB_Komprimieren.TabIndex = 0;
+            RB_Komprimieren.TabStop = true;
+            RB_Komprimieren.Text = "Komprimieren";
+            RB_Komprimieren.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Help
+            // 
+            BTN_Help.Image = Properties.Resources.help;
+            BTN_Help.Location = new Point(684, 12);
+            BTN_Help.Name = "BTN_Help";
+            BTN_Help.Size = new Size(120, 120);
+            BTN_Help.SizeMode = PictureBoxSizeMode.StretchImage;
+            BTN_Help.TabIndex = 1;
+            BTN_Help.TabStop = false;
+            BTN_Help.Click += BTN_Help_Click;
+            // 
+            // BTN_Back
+            // 
+            BTN_Back.Image = Properties.Resources.back;
+            BTN_Back.Location = new Point(12, 12);
+            BTN_Back.Name = "BTN_Back";
+            BTN_Back.Size = new Size(100, 100);
+            BTN_Back.SizeMode = PictureBoxSizeMode.StretchImage;
+            BTN_Back.TabIndex = 2;
+            BTN_Back.TabStop = false;
+            BTN_Back.Click += BTN_Back_Click;
+            // 
+            // BTN_Start
+            // 
+            BTN_Start.Image = Properties.Resources.go1;
+            BTN_Start.Location = new Point(270, 362);
+            BTN_Start.Name = "BTN_Start";
+            BTN_Start.Size = new Size(260, 91);
+            BTN_Start.SizeMode = PictureBoxSizeMode.StretchImage;
+            BTN_Start.TabIndex = 3;
+            BTN_Start.TabStop = false;
+            BTN_Start.Click += BTN_Start_Click;
+            // 
+            // TB_Output
+            // 
+            TB_Output.Location = new Point(152, 293);
+            TB_Output.Name = "TB_Output";
+            TB_Output.PlaceholderText = "Ausgabe";
+            TB_Output.Size = new Size(242, 27);
+            TB_Output.TabIndex = 4;
+            // 
+            // TB_Input
+            // 
+            TB_Input.Location = new Point(152, 252);
+            TB_Input.Name = "TB_Input";
+            TB_Input.PlaceholderText = "Bitte Text oder Binärcode eingeben";
+            TB_Input.Size = new Size(242, 27);
+            TB_Input.TabIndex = 5;
+            // 
+            // TB_PathIn
+            // 
+            TB_PathIn.Location = new Point(434, 252);
+            TB_PathIn.Name = "TB_PathIn";
+            TB_PathIn.PlaceholderText = "Bitte den Werteschlüssel einfügen";
+            TB_PathIn.Size = new Size(242, 27);
+            TB_PathIn.TabIndex = 6;
+            // 
+            // TB_PathOut
+            // 
+            TB_PathOut.Location = new Point(434, 293);
+            TB_PathOut.Name = "TB_PathOut";
+            TB_PathOut.PlaceholderText = "Werteschlüssel";
+            TB_PathOut.Size = new Size(242, 27);
+            TB_PathOut.TabIndex = 7;
+            // 
+            // LBL_Eingabe
+            // 
+            LBL_Eingabe.AutoSize = true;
+            LBL_Eingabe.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            LBL_Eingabe.Location = new Point(22, 243);
+            LBL_Eingabe.Name = "LBL_Eingabe";
+            LBL_Eingabe.Size = new Size(104, 35);
+            LBL_Eingabe.TabIndex = 8;
+            LBL_Eingabe.Text = "Eingabe";
+            // 
+            // LBL_Ausgabe
+            // 
+            LBL_Ausgabe.AutoSize = true;
+            LBL_Ausgabe.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            LBL_Ausgabe.Location = new Point(22, 285);
+            LBL_Ausgabe.Name = "LBL_Ausgabe";
+            LBL_Ausgabe.Size = new Size(112, 35);
+            LBL_Ausgabe.TabIndex = 9;
+            LBL_Ausgabe.Text = "Ausgabe";
+            // 
+            // LBL_Titel
+            // 
+            LBL_Titel.AutoSize = true;
+            LBL_Titel.Font = new Font("Segoe UI", 21F, FontStyle.Regular, GraphicsUnit.Point);
+            LBL_Titel.Location = new Point(300, 24);
+            LBL_Titel.Name = "LBL_Titel";
+            LBL_Titel.Size = new Size(155, 47);
+            LBL_Titel.TabIndex = 10;
+            LBL_Titel.Text = "Huffman";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(400, 252);
+            button1.Name = "button1";
+            button1.Size = new Size(28, 26);
+            button1.TabIndex = 11;
+            button1.Text = "?";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(682, 251);
+            button2.Name = "button2";
+            button2.Size = new Size(28, 29);
+            button2.TabIndex = 12;
+            button2.Text = "?";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(400, 293);
+            button3.Name = "button3";
+            button3.Size = new Size(28, 29);
+            button3.TabIndex = 13;
+            button3.Text = "?";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(682, 293);
+            button4.Name = "button4";
+            button4.Size = new Size(28, 29);
+            button4.TabIndex = 14;
+            button4.Text = "?";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(684, 429);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(105, 24);
+            checkBox1.TabIndex = 15;
+            checkBox1.Text = "Dark Mode";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Huffman
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(816, 507);
+            Controls.Add(checkBox1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(LBL_Titel);
+            Controls.Add(LBL_Ausgabe);
+            Controls.Add(LBL_Eingabe);
+            Controls.Add(TB_PathOut);
+            Controls.Add(TB_PathIn);
+            Controls.Add(TB_Input);
+            Controls.Add(TB_Output);
+            Controls.Add(BTN_Start);
+            Controls.Add(BTN_Back);
+            Controls.Add(BTN_Help);
+            Controls.Add(groupBox1);
+            Name = "Huffman";
+            Tag = "";
+            Text = "Huffman";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)BTN_Help).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BTN_Back).EndInit();
+            ((System.ComponentModel.ISupportInitialize)BTN_Start).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private RadioButton RB_Dekomprimieren;
+        private RadioButton RB_Komprimieren;
+        private PictureBox BTN_Help;
+        private PictureBox BTN_Back;
+        private PictureBox BTN_Start;
+        private TextBox TB_Output;
+        private TextBox TB_Input;
+        private TextBox TB_PathIn;
+        private TextBox TB_PathOut;
+        private Label LBL_Eingabe;
+        private Label LBL_Ausgabe;
+        private Label LBL_Titel;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private CheckBox checkBox1;
+    }
+}
---
+++ Huffman.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuffmanCode
+{
+    public partial class Huffman : Form
+    {
+        public Huffman()
+        {
+            InitializeComponent(); // Initialisiert die Komponenten
+        }
+
+        // Zurück Button Logik
+        private void BTN_Back_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Schließt das Fenster
+        }
+
+        // Hilfetexte
+        private void BTN_Help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wählen Sie im oberen Bereich aus ob Sie einen Text Komprimieren oder einen Binärcode mit dem Werteschlüssel Dekomprimieren wollen.\nIm unteren Bereich müssen Sie die gewünschte Eingabe eingeben.\r\n", "Hilfe"); // Hilfetext
+        }
+
+        // Start Button Logik
+        private void BTN_Start_Click(object sender, EventArgs e)
+        {
+            if (TB_Input.Text == "")
+            {
+                MessageBox.Show("Eingabe ist leer!");                                      // Fehlermeldung, wenn keine Eingabe vorhanden ist
+            }
+
+            else if ((RB_Dekomprimieren.Checked == true && CheckingInput() == false))
+            {
+                MessageBox.Show("Ungültige Eingabe!");                                      // Fehlermeldung, wenn eine falsche Eingabe vorhanden ist
+            }
+
+            else
+            {
+                // Komprimieren
+                if (RB_Komprimieren.Checked == true && RB_Dekomprimieren.Checked == false)
+                {
+                    string? text = TB_Input.Text;                                           // Text wird eingelesen
+                    string? encodedText = Encode(text);                                     // Text wird verschlüsselt
+                    TB_Output.Text = encodedText;                                           // Ausgabe des komprimierten Textes
+
+                    string pathTable = GeneratePathTable(text);                             // Pfadtabelle wird erstellt
+                    TB_PathOut.Text = pathTable;                                            // Ausgabe der Pfadtabelle
+                    TB_PathIn.Text = "";                                                    // Eingabe der Pfadtabelle wird gelöscht
+                }
+
+                // Dekomprimieren
+                else if (RB_Dekomprimieren.Checked == true && RB_Komprimieren.Checked == false)
+                {
+                    string? input = TB_Input.Text;                                          // Eingabe wird eingelesen
+                    string? pathInput = TB_PathIn.Text;                                     // Pfadtabelle wird eingelesen
+
+                    Dictionary<string, char> pathToLetter = new Dictionary<string, char>(); // Pfadtabelle wird erstellt
+
+                    ParsePathInput(pathInput, pathToLetter);                                // Pfadtabelle wird geparst
+
+                    string? output = Decompress(input, pathToLetter);                       // Text wird dekomprimiert
+
+                    TB_Output.Text = output;                                                // Ausgabe des dekomprimierten Textes
+                    TB_PathOut.Text = "";                                                   // Ausgabe der Pfadtabelle wird gelöscht
+                }
+            }
+        }
+
+        // Input beim Dekomprimieren überprüfen
+        private bool CheckingInput()
+        {
+            for (int i = 0; i < TB_Input.Text.Length; i++)
+            {
+                if (TB_Input.Text[i] != '0' && TB_Input.Text[i] != '1')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        // Pfadtabelle erstellen
+        private string GeneratePathTable(string text)
+        {
+            HuffmanTree tree = new();                                                       // Baum wird erstellt
+            tree.BuildTree(text);
+            Dictionary<char, string> encodingTable = tree.BuildEncodingTable(tree.Root);    // Pfadtabelle wird erstellt
+
+            var values = encodingTable.Select(kv => $"{kv.Key}{kv.Value}");
+            string pathTable = string.Join(',', values);
+
+            return pathTable;                                                               // Pfadtabelle wird zurückgegeben
+        }
+
+        // Entschlüsseln
+        private string Encode(string source)
+        {
+            HuffmanTree tree = new();                                                       // Baum wird erstellt
+            tree.BuildTree(source); 
+            Dictionary<char, string> encodingTable = tree.BuildEncodingTable(tree.Root);    // Pfadtabelle wird erstellt
+
+            string encoded = "";                                                            // Variable wird erstellt
+            foreach (char c in source)
+            {
+                encoded += encodingTable[c];
+            }
+
+            return encoded;                                                                 // Variable wird zurückgegeben
+        }
+
+        // Knoten Klasse
+        public class HuffmanNode
+        {
+            public char Symbol
+            {
+                get; set;
+            }
+            public int Frequency
+            {
+                get; set;
+            }
+            public HuffmanNode Left
+            {
+                get; set;
+            }
+            public HuffmanNode Right
+            {
+                get; set;
+            }
+        }
+
+        // Baum Klasse
+        public class HuffmanTree
+        {
+            public HuffmanNode Root
+            {
+                get; set;
+            }
+
+            public static Dictionary<char, int> BuildFrequencyTable(string source)  // Häufigkeitstabelle wird erstellt
+            {
+                var freqTable = new Dictionary<char, int>();
+                foreach (var chr in source)
+                {
+                    if (!freqTable.ContainsKey(chr))
+                        freqTable.Add(chr, 0);
+                    freqTable[chr]++; 
+                }
+                return freqTable;                                                   // Häufigkeitstabelle wird zurückgegeben
+            }
+
+            public void BuildTree(string source)
+            {
+                var freqTable = BuildFrequencyTable(source);                                                // Häufigkeitstabelle wird erstellt
+                var priorityQueue = new PriorityQueue<HuffmanNode>(Comparer<HuffmanNode>.Create((x, y) => x.Frequency - y.Frequency));  // Prioritätswarteschlange wird erstellt
+
+                foreach (var item in freqTable)
+                {
+                    priorityQueue.Push(new HuffmanNode() { Symbol = item.Key, Frequency = item.Value });    // Knoten werden erstellt
+                }
+
+                while (priorityQueue.Count > 1)                                                             // Knoten werden sortiert
+                {
+                    var left = priorityQueue.Pop(); 
+                    var right = priorityQueue.Pop();
+
+                    var parent = new HuffmanNode()                                                          // Knoten werden erstellt
+                    {
+                        Symbol = '\0',
+                        Frequency = left.Frequency + right.Frequency,
+                        Left = left,
+                        Right = right
+                    };
+
+                    priorityQueue.Push(parent);                                                             // Knoten werden in die Warteschlange gepusht
+                }
+
+                Root = priorityQueue.Pop();                                                                 // Wurzel wird erstellt
+            }
+
+            public Dictionary<char, string> BuildEncodingTable(HuffmanNode node, string prefix = "")    // Pfadtabelle wird erstellt
+            {
+                var table = new Dictionary<char, string>();
+
+                if (node.Left == null && node.Right == null)                                            // Knoten werden erstellt
+                {
+                    table.Add(node.Symbol, prefix);                                                     // Knoten werden in die Pfadtabelle gepusht
+                }
+                else
+                {
+                    if (node.Left != null)                                                              // Knoten werden erstellt
+                    {
+                        foreach (var pair in BuildEncodingTable(node.Left, prefix + "0"))               // Knoten werden in die Pfadtabelle gepusht
+                        {
+                            table.Add(pair.Key, pair.Value);
+                        }
+                    }
+
+                    if (node.Right != null)                                                             // Knoten werden erstellt
+                    {
+                        foreach (var pair in BuildEncodingTable(node.Right, prefix + "1"))              // Knoten werden in die Pfadtabelle gepusht
+                        {
+                            table.Add(pair.Key, pair.Value); 
+                        }
+                    }
+                }
+
+                return table;                                                                           // Pfadtabelle wird zurückgegeben
+            }
+
+            public class PriorityQueue<T> 
+            {
+                private List<T> data;           // Warteschlange wird erstellt
+                private IComparer<T> comparer;  // Vergleich wird erstellt
+
+                public PriorityQueue(IComparer<T> comparer) 
+                {
+                    this.comparer = comparer;   // Vergleich wird erstellt
+                    this.data = new List<T>();  // Warteschlange wird erstellt
+                }
+
+                public void Push(T item)        // Element wird in die Warteschlange gepusht
+                {
+                    data.Add(item); 
+                    int ci = data.Count - 1;    // Element wird erstellt
+                    while (ci > 0)              // Element wird sortiert
+                    {
+                        int pi = (ci - 1) / 2; 
+                        if (comparer.Compare(data[ci], data[pi]) >= 0) break;
+                        T tmp = data[ci]; data[ci] = data[pi]; data[pi] = tmp;
+                        ci = pi; 
+                    }
+                }
+
+                public T Pop()          // Element wird aus der Warteschlange gepopt
+                {
+                    T frontItem = data[0]; 
+                    int li = data.Count - 1;
+                    data[0] = data[li];
+                    data.RemoveAt(li);
+
+                    --li;
+                    int pi = 0; 
+                    while (true)        // Element wird sortiert
+                    {
+                        int ci = pi * 2 + 1;
+                        if (ci > li) break;
+                        int rc = ci + 1;
+                        if (rc <= li && comparer.Compare(data[rc], data[ci]) < 0) ci = rc;
+                        if (comparer.Compare(data[pi], data[ci]) <= 0) break;
+                        T tmp = data[pi]; data[pi] = data[ci]; data[ci] = tmp;
+                        pi = ci;
+                    }
+                    return frontItem;   // Element wird zurückgegeben
+                }
+
+                public int Count 
+                {
+                    get
+                    {
+                        return data.Count; // Anzahl der Elemente wird zurückgegeben
+                    }
+                }
+            }
+        }
+
+        // Pfad splitten
+        private void ParsePathInput(string pathInput, Dictionary<string, char> pathToLetter) // Pfad wird gesplittet und in die Pfadtabelle gepusht
+        {
+            string[] entries = pathInput.Split(',', StringSplitOptions.RemoveEmptyEntries); 
+
+            foreach (string entry in entries) 
+            {
+                char letter = entry[0]; 
+                string path = entry[1..];
+
+                pathToLetter[path] = letter;
+            }
+        }
+
+        // Dekomprimieren
+        private string Decompress(string input, Dictionary<string, char> pathToLetter) // Binärcode wird in Text umgewandelt und zurückgegeben
+        {
+            StringBuilder output = new();
+            string currentPath = "";
+
+            foreach (char bit in input)
+            {
+                currentPath += bit;
+
+                if (pathToLetter.ContainsKey(currentPath)) 
+                {
+                    char letter = pathToLetter[currentPath];
+                    output.Append(letter);
+                    currentPath = "";
+                }
+            }
+
+            return output.ToString(); // Text wird zurückgegeben
+        }
+
+        // Hilfetexte
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Geben Sie hier den Text zum Komprimieren ein oder den Binärcode zum Dekomprimieren\r\n", "Hilfe für Eingabe"); // Hilfetexte
+        }
+
+        // Hilfetexte
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Geben Sie hier den Werteschlüssel zum Dekomprimieren ein, beim Kompriemieren leer lassen\r\n", "Hilfe für Eingabe"); // Hilfetexte
+        }
+
+        // Hilfetexte
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hier bekommen Sie beim Komprimieren den Binärcode und beim Dekomprimieren den Originalen Text", "Hilfe für Ausgabe"); // Hilfetexte
+        }
+
+        // Hilfetexte
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hier bekommen Sie beim Komprimieren den WerteSchlüssel, beim Komprimieren ist es leer", "Hilfe für Ausgabe"); // Hilfetexte
+        }
+
+        // Methode für Dark Mode
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) // Dark Mode wird aktiviert oder deaktiviert
+        {
+            if (checkBox1.Checked == true)
+            {
+                BackColor = Color.Black;
+                LBL_Eingabe.ForeColor = Color.White;
+                LBL_Ausgabe.ForeColor = Color.White;
+                LBL_Titel.ForeColor = Color.White;
+                RB_Dekomprimieren.ForeColor = Color.White;
+                RB_Komprimieren.ForeColor = Color.White;
+                checkBox1.ForeColor = Color.White;
+            }
+
+            else
+            {
+                BackColor = Color.White;
+                LBL_Eingabe.ForeColor = Color.Black;
+                LBL_Ausgabe.ForeColor = Color.Black;
+                LBL_Titel.ForeColor = Color.Black;
+                RB_Dekomprimieren.ForeColor = Color.Black;
+                RB_Komprimieren.ForeColor = Color.Black;
+                checkBox1.ForeColor = Color.Black;
+            }
+        }
+    }
+}
